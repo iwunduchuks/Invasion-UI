16fae1144d37a12714e688d7d52d488eee846795
iwunduchuks <iwunduchuks@gmail.com>
2024-12-31 14:49:39 +0100

    Fourth attempt at depednency issue

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
+import sourcemaps from "rollup-plugin-sourcemaps";
 
 const packageJson = require("./package.json");
 
@@ -26,6 +27,7 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      sourcemaps,
     ],
   },
   {
